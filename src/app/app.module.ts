f23b502b78cb03abfbd0585201c0357e1f529212
Luis Arce <luis.arce22@gmail.com>
2019-04-13 08:21:01 -0400

    Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialImportsModule } from './material-imports/material-imports.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule,
     AppRoutingModule
   ],
   providers: [],
