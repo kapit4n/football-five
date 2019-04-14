2b5d11980b5c9f856b543e4201d54b0d7ccde0b6
Luis Arce <luis.arce22@gmail.com>
2019-04-14 18:02:08 -0400

    Add common module components

--- src/app/ccommon/ccommon.module.ts
+++ src/app/ccommon/ccommon.module.ts
@@ -3,17 +3,29 @@ import { CommonModule } from '@angular/common';
 import { MaterialImportsModule } from './../material-imports/material-imports.module'
 
 import {NewslettersComponent } from './newsletters/newsletters.component'
+import { MatchVsComponent } from './match-vs/match-vs.component'
+import { StandingTableGroupsComponent } from './standing-table-groups/standing-table-groups.component'
+import { StandingTableComponent } from './standing-table/standing-table.component'
+import { StandingSortPipe } from './standing-sort.pipe'
 
 @NgModule({
   declarations: [
-    NewslettersComponent
+    NewslettersComponent,
+    MatchVsComponent,
+    StandingTableGroupsComponent,
+    StandingTableComponent,
+    StandingSortPipe,
   ],
   imports: [
     CommonModule,
     MaterialImportsModule
   ],
   exports: [
-    NewslettersComponent
+    NewslettersComponent,
+    MatchVsComponent,
+    StandingTableGroupsComponent,
+    StandingTableComponent,
+    StandingSortPipe,
   ]
 })
 export class CcommonModule { }
